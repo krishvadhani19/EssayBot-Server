534be2f6f1332146e33988c66f20a3d255353292
Krish Vadhani <krishvadhani7@gmail.com>
2025-03-17 19:44:37 -0400

    Validate Token API added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { loginUser } from "../controllers/auth/login";
 import { registerUser } from "../controllers/auth/register";
 import { logoutUser } from "../controllers/auth/logout";
+import { validateToken } from "../controllers/auth/validateToken";
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/validateToken", validateToken);
 
 export default router;
